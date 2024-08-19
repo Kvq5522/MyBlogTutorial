326bdc1f28a60a5ed61a76dc40a0026eb97e3cd0
Kvq5522 <89141917+Kvq5522@users.noreply.github.com>
2024-08-19 13:57:13 +0700

    fix: attempt to fix deploy error

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -7,31 +7,44 @@ export const metadata: Metadata = {
   description: "Let's discover various information!",
 };
 
-const BlogsPage = async () => {
-  const res = await (
-    await fetch(`${process.env.NEXT_PUBLIC_WEBSITE_URL}/blogs/api`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: {
-        revalidate: 60 * 5,
-      },
-    })
-  ).json();
-  const blogs = await new Promise<Array<Blog>>((resolve, reject) => {
-    console.log(res);
-    resolve(
-      res.data?.["items"].map((item: any) => {
-        return {
-          ...item?.["fields"],
-          id: item?.["sys"]?.["id"],
-          image: `https:${item?.["fields"]?.["image"]?.["fields"]?.["file"]?.["url"]}`,
-          content: item?.["fields"]?.["content"]?.["content"],
-        };
+const fetchBlogs = async () => {
+  try {
+    const res = await (
+      await fetch(`${process.env.NEXT_PUBLIC_WEBSITE_URL}/blogs/api`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        next: {
+          revalidate: 60 * 5,
+        },
       })
-    );
-  });
+    ).json();
+    const blogs = await new Promise<Array<Blog>>((resolve, reject) => {
+      try {
+        resolve(
+          res.data?.["items"].map((item: any) => {
+            return {
+              ...item?.["fields"],
+              id: item?.["sys"]?.["id"],
+              image: `https:${item?.["fields"]?.["image"]?.["fields"]?.["file"]?.["url"]}`,
+              content: item?.["fields"]?.["content"]?.["content"],
+            } as Blog;
+          })
+        );
+      } catch (error) {
+        reject([]);
+      }
+    });
+
+    return blogs;
+  } catch (error) {
+    return [];
+  }
+};
+
+const BlogsPage = async () => {
+  const blogs = await fetchBlogs();
 
   return (
     <main>
